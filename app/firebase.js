2e739ae49cbbe6706b75b78dcb6f50f45f707bff
Huz_00 <huzya2929@gmail.com>
2024-08-02 23:39:59 -0400

    changes

--- app/firebase.js
+++ app/firebase.js
@@ -1,21 +1,23 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import {getFirestore} from 'firebase/firestore'
+import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "process.env.AIzaSyCDLNUE3X3SeCft4A6HDJ0zjKoCtpvzOFg",
-  authDomain: "process.env.pantryapp-f37f2.firebaseapp.com",
-  projectId: "process.env.pantryapp-f37f2",
-  storageBucket: "process.env.pantryapp-f37f2.appspot.com",
-  messagingSenderId: "process.env.290620752177",
-  appId: "process.env.1:290620752177:web:e5a22f1917e6bc27a2b010"
+  apiKey: "AIzaSyCDLNUE3X3SeCft4A6HDJ0zjKoCtpvzOFg",
+  authDomain: "pantryapp-f37f2.firebaseapp.com",
+  projectId: "pantryapp-f37f2",
+  storageBucket: "pantryapp-f37f2.appspot.com",
+  messagingSenderId: "290620752177",
+  appId: "1:290620752177:web:e5a22f1917e6bc27a2b010",
 };
 
 // Initialize Firebase.
 
 const app = initializeApp(firebaseConfig);
 const firestore = getFirestore(app)
-export { firestore}
+const auth = getAuth(app);
+export { firestore, auth}
